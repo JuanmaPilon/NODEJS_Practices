5d4b30125094de6fa6c8e296c5c3c0b3f342e892
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-02-07 15:08:22 -0300

    adding server config, made modules and added frontend files

--- NodeJS SG Project/webserver/src/app.js
+++ NodeJS SG Project/webserver/src/app.js
@@ -1,7 +1,14 @@
-const { envs } = require('./config/env');
+//const { envs } = require('./config/env');
+import { envs } from './config/env.js';
+//const { startServer } = require('./server/server.js');
+import { startServer } from './server/server.js';
+
 
 const main = async() => {
-    console.log(envs);
+    startServer({
+        PORT: envs.PORT,
+        PUBLIC_PATH: envs.PUBLIC_PATH
+    });
 }
 
 (async() => {
