e2367df5d2f4ebc71272c6f3afaf3819bdd97a91
JuanmaPilon <juanmartin.pilon@gmail.com>
2025-02-14 12:53:06 -0300

    some more stuff like routes

---
+++ NodeJS SG Project/node_server_ts/src/app.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+
+
+
+
+const app = express();
+
+app.use(express.json());
+
+// Rutas
+
+
+// Auth
+// Usuarios
+
+console.log('Executing app.ts');
+
+export default app;
